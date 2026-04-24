d8cf72f128cce6e4b58b1504f39598d42aa542d1
govanpbadilles-lgtm <govanpbadilles@gmail.com>
2026-04-24 11:47:34 +0800

    node mysql api

---
+++ src/_helpers/db.ts
@@ -0,0 +1,31 @@
+import config from '../../config.json';
+import mysql from 'mysql2/promise';
+import { Sequelize } from 'sequelize';
+import accountModel from '../accounts/account.model';
+import refreshTokenModel from '../accounts/refresh-token.model';
+
+const db: any = {};
+export default db;
+
+initialize();
+
+async function initialize() {
+    // create db if it doesn't already exist
+    const { host, port, user, password, database } = config.database;
+    const connection = await mysql.createConnection({ host, port, user, password });
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+    // connect to db
+    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+
+    // init models and add them to the exported db object
+    db.Account = accountModel(sequelize);
+    db.RefreshToken = refreshTokenModel(sequelize);
+
+    // define relationships
+    db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
+    db.RefreshToken.belongsTo(db.Account);
+
+    // sync all models with database
+    await sequelize.sync();
+}
